05a1a9f23f59daddfd8ee77c94069cb2e006cc62
Andrew Hudson <ahudson@gmail.com>
2019-05-04 13:22:32 +0100

    webhook routes param so don't need loop

--- server.js
+++ server.js
@@ -17,25 +17,41 @@ const ucFirst = string => {
 io.on("connection", socket => {
   socket.emit("server-status", "CONNECTED TO SERVER");
 
-  const people = ['andrew', 'ben', 'dan', 'marcus', 'rajinder'];
-  people.forEach(person => {
-    app.post(`/webhook/${person}`, (req, res) => {
-      console.log(req.body);
-      // if (req.headers['x-serene'] !== 'x-serene') {
-      //   res.status(200);
-      // }
+  app.get(`/webhook/:person`, (req, res) => {
+    console.log(req.params.person);
+    // if (req.headers['x-serene'] !== 'x-serene') {
+    //   res.status(200);
+    // }
 
-      // const {sereneMode} = JSON.parse(Object.keys(req.body)[0]);
+    // const {sereneMode} = JSON.parse(Object.keys(req.body)[0]);
 
-      // const message = {
-      //   sereneMode,
-      //   'user': ucFirst(person),
-      // };
+    // const message = {
+    //   sereneMode,
+    //   'user': ucFirst(person),
+    // };
 
-      // console.log(message);
-      // socket.broadcast.emit("serene-mode", message);
-    });
+    // console.log(message);
+    // socket.broadcast.emit("serene-mode", message);
   });
+
+  // people.forEach(person => {
+  //   app.post(`/webhook/${person}`, (req, res) => {
+  //     console.log(req.body);
+  //     // if (req.headers['x-serene'] !== 'x-serene') {
+  //     //   res.status(200);
+  //     // }
+
+  //     // const {sereneMode} = JSON.parse(Object.keys(req.body)[0]);
+
+  //     // const message = {
+  //     //   sereneMode,
+  //     //   'user': ucFirst(person),
+  //     // };
+
+  //     // console.log(message);
+  //     // socket.broadcast.emit("serene-mode", message);
+  //   });
+  // });
 });
 
 http.listen(PORT, () => {
@@ -44,6 +60,24 @@ http.listen(PORT, () => {
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "frontend/index.html"));
+});
+
+app.get(`/webhook/:person`, (req, res) => {
+  console.log(req.params.person);
+  res.send('hi there ' + req.params.person);
+  // if (req.headers['x-serene'] !== 'x-serene') {
+  //   res.status(200);
+  // }
+
+  // const {sereneMode} = JSON.parse(Object.keys(req.body)[0]);
+
+  // const message = {
+  //   sereneMode,
+  //   'user': ucFirst(person),
+  // };
+
+  // console.log(message);
+  // socket.broadcast.emit("serene-mode", message);
 });
 
 http.on("error", error => {
